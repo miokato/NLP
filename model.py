86a6de17469dc6928f26b4ce38006f7ff4808dce
miokato <miokato@me.com>
2018-04-21 09:28:39 +0900

    [WIP] Add model2

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Reshape
 from keras.layers.wrappers import TimeDistributed
+from seq2seq.models import SimpleSeq2Seq
 
 
 def model_1(input_length, output_length, vocab_size, hidden_size=256):
@@ -13,4 +14,18 @@ def model_1(input_length, output_length, vocab_size, hidden_size=256):
     return model
 
 
+def model_2(input_length, output_length, vocab_size, hidden_size=256):
+    model = Sequential()
+    model.add(SimpleSeq2Seq(
+        input_dim=vocab_size,
+        input_length=input_length,
+        output_length=output_length,
+        output_dim=vocab_size,
+        hidden_dim=hidden_size,
+    ))
+    model.add(Dense(output_length * vocab_size))
+    model.add(Reshape((output_length, vocab_size)))
+    model.add(TimeDistributed(Dense(vocab_size, activation='softmax')))
+
+    return model
 
